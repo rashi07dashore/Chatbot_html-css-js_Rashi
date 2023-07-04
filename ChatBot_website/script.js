3986faa603ff4b5f4359facd0900af9b10b8c9be
Rashi Dashore <119104356+rashi07dashore@users.noreply.github.com>
2023-07-04 19:42:07 +0530

    Add files via upload

---
+++ ChatBot_website/script.js
@@ -0,0 +1,83 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const chatbotCloseBtn = document.querySelector(".close-btn");
+
+let userMessage;
+const API_KEY = "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"; //API_KEY generated from openAI/accounts website I created with another account
+const inputInitHeight= chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+    //Create a chat <li> element with passsed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent= message;
+    return chatLi
+}
+
+const generateResponse = (incomingChatLi) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement= incomingChatLi.querySelector("p");
+
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: userMessage }]
+        })
+    }
+
+    //Send POST request to API, get response
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content;
+
+    }).catch((error) => {
+        messageElement.classList.add("error");
+        messageElement.textContent = "Oops! Something went wrong, try again!";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight))
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();  //getting entered msg and removing extra whitespaces
+    if (!userMessage) return;
+    chatInput.value =""; //the chatbox will be blank once the msg is sent
+    chatInput.style.height = `${inputInitHeight}px`; //resize the msg box once msg is sent
+
+    //Append the user's message to chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        //Display "processing.." msg while waiting for the response
+        const incomingChatLi= createChatLi("Processing...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+chatInput.addEventListener("input", () => {
+    //Adjust the heught of the inout textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    //If Ener key is pressed without Shift key and the window
+    // width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+sendChatBtn.addEventListener("click", handleChat);
+chatbotCloseBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
